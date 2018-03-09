3e583c183281f4029c78133798494ad06c122b07
BAndreas <Brian@rightform.com>
2018-03-09 01:56:39 +0100

    Toevoeging van de Observer en State pattern versie001 Nog niet volledig geimplementeerd

--- printpatterns/src/main/java/com/example/printpatterns/domain/Order.java
+++ printpatterns/src/main/java/com/example/printpatterns/domain/Order.java
@@ -6,7 +6,6 @@ import lombok.Setter;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import java.util.Date;
 
 @Entity
